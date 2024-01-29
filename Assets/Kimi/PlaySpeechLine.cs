8e182e0f0d29b5d2979a1b96b35a6f7e808843f9
KimiJokinen <kimi.jokinen@edu.turkuamk.fi>
2024-01-29 15:19:13 +0200

    Added freeze movement when sitting on chair
    Teleporting to chair disables left thumbstick movement. Pressing primary button unfreezes you.

--- Assets/Kimi/PlaySpeechLine.cs
+++ Assets/Kimi/PlaySpeechLine.cs
@@ -7,16 +7,18 @@ public class PlaySpeechLine : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip audioClip;
     public AudioClip[] audioClips;
+    public bool inProgress;
     void Start()
     {
-
+        inProgress = true;
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && inProgress)
         {
             audioClip = audioClips[Random.Range(0, audioClips.Length)];
             audioSource.PlayOneShot(audioClip);
+            inProgress = false;
         }
     }
 }
